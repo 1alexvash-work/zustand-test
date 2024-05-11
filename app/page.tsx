2f14eae7087509217b69406fdc0fff7a7154df7e
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-05-11 10:32:17 +0300

    Default zustand version

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+import { useCounterStore } from "@/providers/counter-store-provider";
+
+const HomePage = () => {
+  const { count, incrementCount, decrementCount } = useCounterStore(
+    (state) => state
+  );
+
+  return (
+    <div className="flex justify-center ">
+      <div className="flex flex-col gap-1">
+        Count: {count}
+        <button
+          type="button"
+          onClick={decrementCount}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Decrement Count
+        </button>
+        <button
+          type="button"
+          onClick={incrementCount}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Increment Count
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default HomePage;
